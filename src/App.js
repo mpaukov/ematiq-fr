784e4c12e02f6edeec41273ed19c6afe9eddb6d5
Michael Paukov <michael.paukov@gmail.com>
2023-02-24 10:06:27 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
     },
     onMessage: ({ data }) =>
       setData(() => {
-        console.log("Received new data", Date.now().toString());
+        console.log("Received new data", new Date());
         return JSON.parse(data).map((data) => {
           const fullDate = new Date(data.date * 1000);
           const now = new Date().getDate();
